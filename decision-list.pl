bc0e9d0a0ed84669790587590b7f4a00d3990dd9
r-best <bobbyisbest3@gmail.com>
2018-03-19 17:02:51 -0400

    Adding Laplace smoothing & fixing some bugs

--- decision-list.pl
+++ decision-list.pl
@@ -29,8 +29,12 @@ if(open(my $fh, "<:encoding(UTF-8)", $stopwords)){
     die "Error opening '$stopwords'";
 }
 
+# %senses maps each sense to all the words it appears with, and all 
+# of those words to how often they appear with that sense
+# i.e. @senses{sense} = [words]
+#      $senses{sense}{word} = freq
 my %senses;
-my %features;
+my %features; # Maps all features to how often they appear
 
 if(open(my $fh, "<:encoding(UTF-8)", $train)){
     my $text = do { local $/; <$fh> }; # Read in the entire file as a string
@@ -41,12 +45,12 @@ if(open(my $fh, "<:encoding(UTF-8)", $train)){
     my @instances = ($text =~ /(<instance.*?<\/instance>)/gs);
     
     for my $instance (@instances){
-        my $sense = ($instance =~ /senseid=\"(.*)\"/)[0]; # Get its sense
-        my @sentences = ($instance =~ /(<s>(.*?)<\/s>)/gs); # Get all of its sentences (ignoring paragraphs)
+        my $sense = ($instance =~ /senseid=\"(.*)\"/)[0]; # Get the instance's sense
+        my @sentences = ($instance =~ /<s>(.*?)<\/s>/gs); # Get all of its sentences (ignoring paragraphs)
         
         for my $sentence (@sentences){
             $sentence =~ s/<s>|<\/s>|<@>|<p>|<\/p>//gs; # Remove the tags so we're left with those good good words
-            $sentence =~ s/\b$stopwords\b//gs; # Remove stopwords
+            $sentence =~ s/\b($stopwords)\b//gs; # Remove stopwords
             @words = ($sentence =~ /\b\w+\b/gs); # Split sentence into words
 
             for my $word (@words){
@@ -57,44 +61,28 @@ if(open(my $fh, "<:encoding(UTF-8)", $train)){
             }
         }
     }
-
-    # Change the %senses hash from recording frequencies of words appearing
-    # with a sense to probabilities of a word given the sense
+    
+    # Divide each frequency $senses{sense}{word} by the total frequency of that word,
+    # so that the values of %senses{sense}{word} become the probabilities P(sense|word)
     for my $sense (keys %senses){
-        for my $word (keys %{$senses{$sense}}){
-            # P(sense|word) = freq(sense, word) / freq(word)
-            $senses{$sense}{$word} /= %features{$word};
+        for my $word (keys %features){
+            # P(sense|word) = (freq(sense, word)+1) / (freq(word)+(sizeof(@features))
+            $senses{$sense}{$word} = ($senses{$sense}{$word}+1) / (%features{$word}+(0+(keys %features)));
         }
     }
     
     # For each word, calculate the log ratio of the probabilities
     # of the word given each of the two senses
+    # Because of how logs work, if the result is positive it's in
+    # favor of sense 1, if it's negative it's in favor of sense 2
     # (At this point I'm assuming there are only two senses)
     my $sense1 = (keys %senses)[0];
     my $sense2 = (keys %senses)[1];
     for my $word (keys %features){
-        # If the word wasn't seen with either sense, ignore it (this shouldn't even be possible)
-        if(!(exists $senses{$sense1}{$word}) && !(exists $senses{$sense2}{$word})){
-            continue;
-        }
-        # TODO: Maybe use smoothing for this part instead of this arbitrary max value
-        # If the word was never seen with the first sense, set to max value for second sense
-        if(!(exists $senses{$sense1}{$word})){
-            $features{$word} = -1000000;
-        }
-        # If the word was never seen with the second sense, set to max value for first sense
-        elsif(!(exists $senses{$sense2}{$word})){
-            $features{$word} = 1000000;
-        }
-        # Else word was seen with both senses, so do the actual math
-        else{
-            my $probSense1 = $senses{$sense1}{$word};
-            my $probSense2 = $senses{$sense2}{$word};
-            $features{$word} = log($probSense1/$probSense2) / log(2);
-            print $features{$word}."\n";
-        }
+        my $probSense1 = $senses{$sense1}{$word};
+        my $probSense2 = $senses{$sense2}{$word};
+        $features{$word} = log($probSense1/$probSense2) / log(10);
     }
-    print Dumper(@features);
 } else{
     die "Error opening training file '$train'";
 }
